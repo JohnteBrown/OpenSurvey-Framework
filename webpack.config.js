0a752daf692eb040e40b6947104267f8fe1e8420
johntebrown <brown_johnte@student.mahoningctc.com>
2026-05-10 12:58:15 -0400

    edited webpack.config.js to include master.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/master.js",
 
   output: {
     filename: "bundle.js",
